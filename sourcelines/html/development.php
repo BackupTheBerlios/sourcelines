e26a684f376580cee9d76000e8567ca1d425d3a4
helix <helix>
2002-01-04 13:53:45 +0000

    add html/*.php

---
+++ sourcelines/html/development.php
@@ -0,0 +1,57 @@
+<?php
+
+######################################################################
+# SourceLines: Open Source Solutions
+# ===================================================================
+#
+# Copyright (c) 2001 by
+#                Lutz Henckel (lutz.henckel@fokus.gmd.de) and
+#                Norbert Geiges
+#
+# BerliOS SourceLines: http://sourcelines.berlios.de
+# BerliOS - The OpenSource Mediator: http://www.berlios.de
+#
+# The SourceLines Project Page for developers
+#
+# It also shows the number of apps in each one
+# This program is free software. You can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 or later of the GPL.
+###################################################################### 
+
+require("header.inc");
+
+?>
+
+<!-- content -->
+
+<P><H2>For developers</H2>
+
+<P>SourceLines is Open Source, so you're invited to contribute to it.
+
+<P>SourceLines has been thought as a documentation and retrieval system for Open Source Solutions, but maybe you're interested in adapting SourceLines to document other kind of things. Or maybe you want to improve
+SourceLines adding features or correcting errors. If one of these is your case, keeping on reading will be of interest for you.
+
+<P>The documentation that is included in the <A HREF="http://developer.berlios.de/project/filelist.php?group_id=91">SourceLines package</A> has been made with the idea of a developer who wants to
+make further development in SourceLines or who wants to make some
+modifications so that SourceLines suits his needs.
+
+<P>Note: For developing or modifying SourceLines, <A HREF="http://www.php.net">PHP</A> knowledge is needed. You should also have
+certain SQL experience if you want to handle with databases.
+
+<P>Please report to the <A HREF="authors.php3">authors</A> any adaptions and/or modifications. This
+could help us knowing what SourceLines should implement in the future
+and making other SourceLines users benefit from your work.
+
+<P>For advanced uses, <A HREF="http://phplib.netuse.de/">PHPLib</A> should also be known. You can read about
+PHPLib's API in PHPLib's manual. At this moment, PHPLib is used only
+for sessions and authentication. If you aren't going to work with
+them, you don't need to have a look at PHPLib.
+
+<P>&nbsp;
+
+<!-- end content -->
+
+<?php
+require("footer.inc");
+?>
